359de08cd043c8fe303e116b0a11201599c19369
balzer <antonbaltesr@yahoo.com>
2023-06-10 18:58:31 +0000

    func.apply

--- JSdecorations.js
+++ JSdecorations.js
@@ -29,7 +29,7 @@
 
  console.log(slow(2));  //кешируем
  console.log("Again:  " + slow(2))  //возвращаем из кеша
-
+/*****-------------------------------------------------------------*****/
  // Применение "func.call" для передачи контента
 
  let worker = {
@@ -59,3 +59,33 @@
 
  console.log( worker.slow(2)    ); // работает
  console.log( worker.slow(2) ); // работает, не вызывая первоначальную функцию (кешируется)
+ /*****-------------------------------------------------------------*****/
+ // несколько аргументов "func.apply"
+
+ let workerCall = {
+        slow(min, max) {
+            console.log(`called with ${min}, ${max}`);
+            return min + max;
+        }
+     };
+
+ function cachingDecoratorCall2 (func, hash) {
+     let cache = new Map();
+     return function () {
+         let key = hash(arguments);
+         if(cache.has(key)) {
+             return cache.get(key);
+         }
+         let result = func.call(this, ...arguments);   // === func.apply(this, arguments)
+
+         cache.set(key, result);
+         return result;
+     }
+ }
+ function hash(args) {
+     return args[0] + ',' + args[1];
+ }
+ workerCall.slow = cachingDecoratorCall2(workerCall.slow, hash)
+
+ console.log(workerCall.slow(3, 5))
+ console.log('Again', workerCall.slow(3, 5))
